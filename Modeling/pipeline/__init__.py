9d4c39cb6de1eb23b935d4601cd7a64bbdb5d02f
MJCHO <jmj3078@gmail.com>
2026-07-28 13:46:16 +0900

    legacy : non-mixture model

--- Modeling/pipeline/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pathlib import Path
-
-_MODELING = Path(__file__).resolve().parent.parent
-_ROOT = _MODELING.parent
-for _p in (str(_ROOT), str(_MODELING)):
-    if _p not in sys.path:
-        sys.path.insert(0, _p)
